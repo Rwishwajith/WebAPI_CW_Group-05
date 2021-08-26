1d3d3c723ed8f6cfc8941c969ce80105d7e1657c
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-26 23:05:22 +0530

    Create Schema and Model and Schema for Main Category Section

---
+++ models/maincategory.js
@@ -0,0 +1,12 @@
+//**************LOG HISTORY ***********************
+//26.08.2021        Ruchira Wishwajith        Created.
+//26.08.2021        Ruchira Wishwajith        Created Model for main Category
+const mongoose = require('mongoose')
+
+const mainCategorySchema = require('../schema/maincategory').mainCategorySchema
+
+const MainCategory = mongoose.model('MainCategory',mainCategorySchema)
+
+exports.MainCategory = MainCategory
+
+exports.mongoose = mongoose
