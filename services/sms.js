9137b03a785047c17bd4f1b193c8710bd22b0f4b
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-27 01:55:13 +0530

    Created Service SMS

---
+++ services/sms.js
@@ -0,0 +1,24 @@
+const config = require('config')
+const https = require('https');
+
+const axios = require('axios')
+
+function sendSms(mobileNumber,content){
+    return new Promise((resolve,reject)=>{
+        try{
+            let url = config.get('sms.url')
+            let body = {
+                user_id: config.get('sms.userId'),
+                api_key: config.get('sms.apiKey'),
+                sender_id: config.get('sms.senderId'),
+                to: mobileNumber.toString(),
+                message: content
+            }
+            axios.post(url,body).then(_ => {return resolve(true)}).catch(error => {return reject(error)})
+        }catch(e){
+            return reject(e)
+        }
+    })
+}
+
+exports.sendSms=sendSms
