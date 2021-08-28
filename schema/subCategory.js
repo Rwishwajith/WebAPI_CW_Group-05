6774b899b3366136b34323c63515acc3631b1371
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-28 13:38:55 +0530

    Bug Fixes

--- schema/subCategory.js
+++ schema/subCategory.js
@@ -2,11 +2,12 @@
 //**************LOG HISTORY ***********************
 //24.08.2021        Ruchira Wishwajith        Created.
 //24.08.2021        Ruchira Wishwajith        Created Scehma for Sub Category
+//28.08.2021        Ruchira Wishwajith        Updated the referance for MainCategory
 
 const subCategorySchema = new mongoose.Schema({
     masterCategoryId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:'MasterCategory'
+        ref:'MainCategory'
     },
     name:{
         type:String
