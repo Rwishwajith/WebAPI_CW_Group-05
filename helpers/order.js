9d4f39036e1f199abc68591b41e851f50d4acd31
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-29 15:33:10 +0530

    Update order.js helper

--- helpers/order.js
+++ helpers/order.js
@@ -1,9 +1,58 @@
 /***************LOG HISTORY ***********************
 29.08.2021        Ruchira Wishwajith        Created the file.
 29.08.2021        Ruchira Wishwajith        Added function getallforusers(UserID)
+29.08.2021        Ruchira Wishwajith        Added function to GetAll()
+30.08.2021        Ruchira Wishwajith        Added function to getOneForUser(userId,orderId)
+30.08.2021        Ruchira Wishwajith        Added function to  getOne(orderId)
+30.08.2021        Deshani Rajapaksha        Added function to  addOne(data)updateOne(data)
+30.08.2021        Ruchira Wishwajith        Added function to updateOne(data)
+30.08.2021        Ruchira Wishwajith        Code Refctoring
+
 */
+const orderModel = require('../models/order')
+const cartModel = require('../models/cart')
+const cartItemModel = require('../models/cartItem')
+const userModel = require('../models/user')
+const productModel = require('../models/product')
 
+const email = require('../services/email')
 
+const ORDERSTATUS = {
+    0:"Pending",
+    1:"Accept",
+    2:"Proccessing",
+    3:"Shipped",
+    4:"Complete"
+}
+
+function getAll(){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let orders = await orderModel.Order.find().populate([{
+                path: 'user',
+                model: 'User'
+            }, {
+                path: 'cart',
+                model: 'Cart',
+                populate: {
+                    path: 'items',
+                    model: 'CartItem',
+                    populate: {
+                        path: 'product',
+                        model: 'Product',
+                        populate: {
+                            path: 'images',
+                            model: 'ProductImage'
+                        },
+                    },
+                },
+            }])
+            return resolve(orders)
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
 
 function getAllForUser(userId){
     return new Promise(async(resolve,reject)=>{
@@ -33,3 +82,125 @@ function getAllForUser(userId){
         }
     })
 }
+
+function getOneForUser(userId,orderId){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let orders = await orderModel.Order.find({_id:new orderModel.mongoose.Types.ObjectId(orderId),user:new orderModel.mongoose.Types.ObjectId(userId)}).populate('user','cart')
+
+            return resolve(orders)
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
+
+function getOne(orderId){
+    return new Promise(async(resolve,reject)=>{
+        try{
+        
+            let order = await orderModel.Order.findOne({_id:new orderModel.mongoose.Types.ObjectId(orderId)}).populate('user','cart')
+
+            if(!order)
+                return reject({message:null,error:"Unable to find order for provided order id",code:404,data:null})
+
+            return resolve(product)
+
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
+
+function addOne(data){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let userData = await userModel.User.findOne({_id:data.user})
+            
+            if(!userData)
+                return reject({message:"Unable to get user data",error:null,code:404,data:null})
+
+            let order=new orderModel.Order({
+                user:new orderModel.mongoose.Types.ObjectId(data.user),
+                total:data.total,
+                status:data.status
+            })
+
+            let cart = new cartModel.Cart({
+                order:new orderModel.mongoose.Types.ObjectId(order._id),
+                items:[]
+            })
+
+            new Promise(async(resolve, reject) => {
+                for(const cartProductData of data.cart){
+                    let cartItemData = new cartItemModel.CartItem({
+                        cart: new cartModel.mongoose.Types.ObjectId(cart._id),
+                        product:new productModel.mongoose.Types.ObjectId(cartProductData.productId),
+                        qty:cartProductData.qty,
+                    })
+                    await cartItemData.save()
+                    cart.items.push(cartItemData._id)
+                }
+                return resolve(true)
+            }).then(()=>{
+                cart.save().then((res)=>{
+                    order.cart=new cartModel.mongoose.Types.ObjectId(cart._id)
+                    order.save().then((res)=>{
+                        email.sendEmail(userData.email,"Order Placed",`Your order is successfully placed. Order ID is ${order.id}`).then(()=>{
+                            return resolve("Order successfully saved")
+                        }).catch((e)=>{
+                            return reject({message:"Unable to send email",error:e.message,code:424,data:null})
+                        })
+                    }).catch((e)=>{
+                        return reject({message:"Unable to save to database",error:e.message,code:500,data:null})
+                    })
+                })
+            }).catch((e)=>{
+                return reject({message:"Unable to add cart item data",error:e.message,code:400,data:null})
+            })
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
+
+function updateOne(data){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let order = await orderModel.Order.findOne({_id:new orderModel.mongoose.Types.ObjectId(data.order)})
+
+            if(!order)
+                return reject({message:null,error:'Unable to find order',code:404,data:null})
+
+            let userData = await userModel.User.findOne({_id:order.user})
+
+            if(!userData)
+                return reject({message:null,error:'Unable to find user data',code:404,data:null})
+
+            order.status=data.status
+
+            order.save().then((res)=>{
+                email.sendEmail(userData.email,"Order Placed",`Your order is now on ${ORDERSTATUS[data.status]} state. Thank you.`).then(()=>{
+                    return resolve("Order successfully saved")
+                }).catch((e)=>{
+                    return reject({message:"Unable to send email",error:e.message,code:424,data:null})
+                })
+            }).catch((e)=>{
+                return reject({message:"Unable to save to database",error:e.message,code:500,data:null})
+            })
+            
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500})
+        }
+    })
+}
+
+
+exports.getAll = getAll
+exports.getAllForUser=getAllForUser
+exports.getOneForUser=getOneForUser
+exports.getOne = getOne
+exports.addOne = addOne
+exports.updateOne = updateOne
+exports.deleteAll = deleteAll
+exports.deleteOne = deleteOne
