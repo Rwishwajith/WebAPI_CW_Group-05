d0b1aac8974bb4b342d1825974d6a902da0cb944
Deshiwathsala <deshaniwathsala97@gmail.com>
2021-08-30 01:34:14 +0530

    Created a POST Method to add a Main Category.

--- helpers/category.js
+++ helpers/category.js
@@ -2,6 +2,7 @@
 28.08.2021        Ruchira Wishwajith        Created the file.
 28.08.2021        Ruchira Wishwajith        Created the relationship with maincategory and subcategory.
 29.08.2021        Sandaruwani Weerasinghe   Created get all method
+30.08.2021        Deshani Rajapaksha        Created a POST method to add Main Category.
 */
 
 const maincategoryModel = require('../models/maincategory')
@@ -22,3 +23,23 @@ function getAll(){
     })
 }
 
+function addNewMainCategory(name){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let isExist = await mainCategoryModel.MainCategory.count({name:name}) == 0?false:true
+
+            if(isExist)
+                return reject({message:"Category already exists",error:e.message,code:409,data:null})
+
+            let masterCategory = new mainCategoryModel.MainCategory({
+                name:name
+            })
+
+            mainCategory.save().then((res)=>{
+                return resolve({mainCategoryId:mainCategory._id})
+            }).catch((e)=>{return reject({message:"Unable to save to database",error:e.message,code:500,data:null})})
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
