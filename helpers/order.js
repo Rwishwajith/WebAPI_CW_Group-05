b0f926c765926cc526b188c0f2b5072af42995a6
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-29 10:47:29 +0530

    Function for GetAllForUsers - helper/Order

--- helpers/order.js
+++ helpers/order.js
@@ -1,3 +1,35 @@
 /***************LOG HISTORY ***********************
 29.08.2021        Ruchira Wishwajith        Created the file.
+29.08.2021        Ruchira Wishwajith        Added function getallforusers(UserID)
 */
+
+
+
+function getAllForUser(userId){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let orders = await orderModel.Order.find({user:new orderModel.mongoose.Types.ObjectId(userId)}).populate([{
+                path: 'user',
+                model: 'User'
+            }, {
+                path: 'cart',
+                model: 'Cart',
+                populate: {
+                    path: 'items',
+                    model: 'CartItem',
+                    populate: {
+                        path: 'product',
+                        model: 'Product',
+                        populate: {
+                            path: 'images',
+                            model: 'ProductImage'
+                        },
+                    },
+                },
+            }])
+            return resolve(orders)
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
