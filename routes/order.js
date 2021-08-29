ffe84dc76cb5165da57ee0217b7880933cd87982
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-08-29 22:17:12 +0530

    Created Order(GET) method

--- routes/order.js
+++ routes/order.js
@@ -2,6 +2,7 @@
 29.08.2021        Ruchira Wishwajith        Created.
 29.08.2021        Deshani Rajapaksha        Created POST Method to add Orders.
 29.08.2021        Ruchira Wishwajith        Created GET method to retrive user's orders
+29.08.2021        Ruchira Wishwajith        Created GET method to retrive orders
 */
 
 
@@ -14,6 +15,24 @@
 
             order.getAllForUser(userId).then((orders)=>{
                 return respond.status(200).send({success:true,message:'Orders successfully fetched',error:null,code:200,data:orders})
+            }).catch((e)=>{
+                return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
+            })
+        }catch(e){
+            return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,code:500,data:null})
+        }
+    })
+
+    routes.get('/user/:orderId',jwtMiddleware,(request, respond)=>{
+        try{
+            let userId = request.user.id
+            let orderId = request.params.orderId
+
+            if(!validator.validateEmptyFields(orderId))
+                return respond.status(200).send({success:false,message:'Missing or empty required fields',error:null,code:400,data:null})
+
+            order.getOneForUser(userId,orderId).then((orders)=>{
+                return respond.status(200).send({success:true,message:'Order successfully fetched',error:null,code:200,data:orders})
             }).catch((e)=>{
                 return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
             })
