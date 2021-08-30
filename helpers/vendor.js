d02ba97a2d3533227183e6c1c70d3b97f200828d
s-weerasinghe <weerasinghesandaruwani@gmail.com>
2021-08-30 22:26:01 +0530

    get all for vendors

--- helpers/vendor.js
+++ helpers/vendor.js
@@ -1,3 +1,16 @@
 /***************LOG HISTORY ***********************
 29.08.2021        Ruchira Wishwajith        Created the file.
+30.08.2021        Sandaruwani Weerasinghe   get all for vendors.
 */
+
+function getAll(){
+    return new Promise(async(resolve,reject)=>{
+        try{
+            let vendors = await vendorModel.Vendor.find()
+        
+            resolve(vendors)
+        }catch(e){
+            return reject({message:"Undetected error",error:e.message,code:500,data:null})
+        }
+    })
+}
