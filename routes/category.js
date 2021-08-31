e4afcef9dc67531470c2d3f6c99a39d256455dbf
Deshiwathsala <deshaniwathsala97@gmail.com>
2021-08-31 22:15:59 +0530

    Created a POST Method for MasterCategory

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 /**************LOG HISTORY ***********************
 29.08.2021        Ruchira Wishwajith        Created.
 29.08.2021        Ruchira Wishwajith        Created GET Method to fecth categories
+31.08.2021        Deshani Rajapaksha        Created a POST Method for MasterCategory
 */
 
 
@@ -20,4 +21,19 @@ module.exports = (()=>{
         }
     })
 
-   
+    routes.post('/masterCategory',jwtMiddleware,checkAdminPermissions,(request, respond)=>{
+        try{
+            let name = request.body.name
+
+            if(!validator.validateEmptyFields(name))
+                return respond.status(200).send({success:false,message:'Missing or empty required fields',error:null,data:null})
+
+            category.addNewMasterCategory(name).then((products)=>{
+                return respond.status(200).send({success:true,message:'Master category successfully added',error:null,code:200,data:products})
+            }).catch((e)=>{
+                return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
+            })
+        }catch(e){
+            return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,data:null})
+        }
+    })
