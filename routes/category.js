eccdc88ed3ac811f229e8669a19b462ac13ac8d2
Deshiwathsala <deshaniwathsala97@gmail.com>
2021-08-31 23:04:47 +0530

    Created a POST Method for SubCategory

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,7 @@
 29.08.2021        Ruchira Wishwajith        Created.
 29.08.2021        Ruchira Wishwajith        Created GET Method to fecth categories
 31.08.2021        Deshani Rajapaksha        Created a POST Method for MasterCategory
+01.09.2021        Deshani Rajapaksha        Created a POST Method for SubCategory.
 */
 
 
@@ -30,6 +31,24 @@ module.exports = (()=>{
 
             category.addNewMasterCategory(name).then((products)=>{
                 return respond.status(200).send({success:true,message:'Master category successfully added',error:null,code:200,data:products})
+            }).catch((e)=>{
+                return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
+            })
+        }catch(e){
+            return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,data:null})
+        }
+    })
+
+    routes.post('/subCategory',jwtMiddleware,checkAdminPermissions,(request, respond)=>{
+        try{
+            let masterCategoryId = request.body.masterCategoryId
+            let name = request.body.name
+
+            if(!validator.validateEmptyFields(masterCategoryId,name))
+                return respond.status(200).send({success:false,message:'Missing or empty required fields',error:null,data:null})
+
+            category.addNewSubCategory(masterCategoryId,name).then((products)=>{
+                return respond.status(200).send({success:true,message:'Sub category successfully added',error:null,code:200,data:products})
             }).catch((e)=>{
                 return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
             })
