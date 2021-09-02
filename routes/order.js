d27fb2644a0a741d239127e243478ddeb179434c
Mpabasara <miththampi@gmail.com>
2021-09-02 20:47:56 +0530

    Create a method for DeleteId

--- routes/order.js
+++ routes/order.js
@@ -32,6 +32,23 @@
         }
     })
 
+    routes.delete('/:id',jwtMiddleware,checkAdminPermissions,(request, respond)=>{
+        try{
+            let orderId=request.params.id
+
+            if(!validator.validateEmptyFields(orderId))
+                return respond.status(200).send({success:false,message:'Missing or empty required fields',error:null,code:400,data:null})
+
+            order.deleteOne(orderId).then((result)=>{
+                return respond.status(200).send({success:true,message:'Order successfully deleted',error:null,code:200,data:result})
+            }).catch((e)=>{
+                return respond.status(200).send({success:false,message:e.message,error:e.error,code:e.code,data:e.data})
+            })
+        }catch(e){
+            return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,code:500,data:null})
+        }
+    })
+
     routes.get('/user/',jwtMiddleware,(request, respond)=>{
         try{
             let userId = request.user.id
